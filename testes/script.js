239a33c743b442594dfa6fa7ebb51d716f9d2428
Felipe Harlen Santos de Sousa <felipeharlen@hotmail.com>
2020-11-07 02:00:34 -0300

    issue #1 e #3
    Resolução das issues #1 e #3. A issue #2 também está presente, mas não terminada.

---
+++ testes/script.js
@@ -0,0 +1,35 @@
+class Leve{
+    #element;
+    #var1;
+    #states;
+    #statesVar1;
+    #span;
+    constructor(id){
+        this.#element = document.querySelector(id);
+        this.#span = this.#element.querySelector('span')
+        this.#var1 = '[[var1]]'
+        this.#statesVar1 = [];
+        this.#states = [];
+    }
+    
+    // issue #1
+    subs(str){
+        this.#states.unshift(this.#element.innerText);
+        this.#statesVar1.unshift(this.#var1);
+        this.#element.innerText = this.#element.innerText.replaceAll(this.#var1, str);
+        this.#var1 = str;
+    }
+
+    //issue #2 (incompleta)
+    input(str){   
+        this.#span.setAttribute('l:var', str);
+    }
+
+    //issue #3
+    undo(){
+        if(this.#states.length == 0)
+            throw "Não exitem estados anteriores a serem restaurados."
+        this.#element.innerText = this.#states.shift();
+        this.#var1 = this.#statesVar1.shift();
+    }
+}
