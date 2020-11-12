0e7d528ad5ed358ad29b64fa9fc9121af23e5adb
marcosrodrigues1777 <marcosrodrigues1777@gmail.com>
2020-11-12 18:02:28 -0300

    #issue1, #issue2

--- testes/script.js
+++ testes/script.js
@@ -1,12 +1,12 @@
 
 class Alterar{
-			//issue 1
-  	 mudarId( id,mudanca){
+	//#issue 1
+  	mudarId( id,mudanca){
    	 		document.getElementById(id).innerHTML=mudanca;
  		 }
      
-     //issue 2
-  	 mudarVariavel(id,variavel,mudanca){
+    //#issue 2
+  	mudarVariavel(id,variavel,mudanca){
 			  var str = document.getElementById("app").innerHTML; 
 			  var res = str.replace(variavel, mudanca);
   			document.getElementById(id).innerHTML = res;
@@ -14,18 +14,16 @@ class Alterar{
 }
 
 
-
 class App {
     constructor(){
     }
-    
     run(){
 		let obj= new Alterar();
-    obj.mudarVariavel('app','var1','Mudou');
-
+    	obj.mudarVariavel('app','var1','Mudou');
 	}
 }
+
 window.onload = function() {
-app = new App();
-app.run();
+	app = new App();
+	app.run();
 }
