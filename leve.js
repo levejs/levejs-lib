bc8193f87f0630602b04a5336f5ce1a565fc6da7
Mário Barreto <m4riobarreto@gmail.com>
2020-11-28 12:08:40 -0300

    Issues #1, #2, #3 e #4

--- leve.js
+++ leve.js
@@ -1,47 +1,99 @@
-// Classe que permitirá manipular o HTML
+// Classe que permitirá manipular o HTML.
 class Leve {
     // O método construtor receberá o id do elemento que será manipulado, e um objeto com um "mapa de variáveis".
     constructor(id, mapaDeVariaveis) {
-        this._elemento = document.getElementById(id);       //Armazena o elemento de id informado.
-        this._memento = this._elemento.innerHTML;           //Armazena o estado original daquele elemento.
-        this._variaveis = mapaDeVariaveis;                  //Armazena o mapa de variáveis (propriedades) de um objeto.
-
+        this._elemento = document.getElementById(id);       // Armazena o elemento de id informado.
+        this._memento = this._elemento.innerHTML;           // Armazena o estado original daquele elemento.
+        this._variaveis = mapaDeVariaveis;                  // Armazena o mapa de variáveis (propriedades) de um objeto.
+        
         // Percorre os nomes das variáveis.
-        for(let nomeVar of Object.keys(this._variaveis)){
-            // Vincula cada variável à instância atual, permitindo obter e atualizar o seu valor quando ocorrer alguma mudança.
+        for(let nomeVar of Object.keys(this._variaveis)) {
+            // Vincula cada variável à instância atual, permitindo obter (get), atualizar (set) e renderizar "this.atualizarElemento()" o seu valor ao ocorrerem mudanças nas variáveis.
             Object.defineProperty(this, nomeVar, {
                 get: () => {
                     return this._variaveis[nomeVar];
                 },
-                set: (valor) => { 
+                set: (valor) => {
                     this._variaveis[nomeVar] = valor;
-                    this.substituirVariaveis();
+                    this.atualizarElemento();
                 }
             });
         }
 
-        this.substituirVariaveis();
+        this.sincronizarInputs();
 
+        this.atualizarElemento();
+        
     }
 
-    // Executa os métodos de substituição de variáveis.
-    substituirVariaveis() {
-        this.substituirVariaveisMustache();
+    // Sincroniza o conteúdo dos inputs de texto com as variáveis do elemento.
+    sincronizarInputs() {
+        let pai = this._elemento;
+        let filhos = pai.getElementsByTagName("input");
+
+        pai.addEventListener("input", (evento) => {
+            filhos = evento.target;
+            this[filhos.getAttribute("l:bind")] = filhos.value;
+        });
+        
+    }
+
+    // Executa os métodos de renderização de variáveis, mantendo o foco no input de texto utilizado pelo usuário.
+    atualizarElemento() {
+        // Armazena o elemento que possuia o foco do usuário, antes de renderizar a mudança numa variável.
+        // Isso deve ser feito para que o foco no input de texto seja restaurado mais tarde.
+        let foco = document.activeElement.id;
+
+        let pai = this._elemento;
+        let filhos = pai.getElementsByTagName("input");
+
+        // Armazena o conteúdo dos inputs de texto num array, antes de renderizar a mudança numa variável.
+        // Isso deve feito para que o conteúdo digitado pelo usuário seja restaurado mais tarde.
+        let conteudoInputs = [];
+        for(let input of filhos) {
+            if(input.getAttribute("l:bind") != undefined) {
+                conteudoInputs.push(input.value);
+            }
+        }
+
+        // Restaura o estado original do elemento para que o método "substituirVariaveisMarcadas()" funcione corretamente.
+        this._elemento.innerHTML = this._memento;
+
+        // Métodos de renderização de variáveis.
+        this.substituirVariaveisMarcadas();
         this.substituirVariaveisSpan();
+
+        // Restaura o conteúdo dos inputs de texto, a medida que a mudança numa variável é renderizada. Com isso, o conteúdo já digitado pelo usuário irá permanecer no input de texto enquanto ele está digitando.
+        let i = 0;
+        for(let input of filhos) {
+            if(input.getAttribute("l:bind") != undefined) {
+                input.value = conteudoInputs[i];
+                i++;
+            }
+        }
+        
+        // Restaura o foco no elemento, a medida que a mudança numa variável é renderizada. Com isso, o input de texto irá permanecer em foco enquanto o usuário está digitando.
+        if(foco != '') {
+            document.getElementById(foco).focus();
+        }
+        
     }
 
-    // Substitui todo o conteúdo de um elemento.
+    // Substitui todo o conteúdo de um elemento por um novo conteúdo.
     substituirConteudo(novoConteudo) {
         this._elemento.innerHTML = novoConteudo;
     }
 
-    // Renderiza os valores das variáveis no lugar de variáveis Mustache que possuírem o mesmo nome da variável.
-    substituirVariaveisMustache() {
-        let template = this._memento;
+    // Renderiza os valores das variáveis no lugar de variáveis marcadas com colchetes "[[variavel]]" que possuírem o mesmo nome da variável.
+    substituirVariaveisMarcadas() {
+        // Percorre os nomes das variáveis.
+        for(let nomeVar of Object.keys(this._variaveis)) {
 
-        let conteudoAtualizado = Mustache.render(template, this._variaveis);
+            let conteudoAtualizado = this._elemento.innerHTML.replaceAll(`[[${nomeVar}]]`, this[nomeVar]);
 
-        this.substituirConteudo(conteudoAtualizado);
+            this.substituirConteudo(conteudoAtualizado);
+        }
+
     }
 
     // Renderiza os valores das variáveis dentro do conteúdo de tags <span> que possuírem o valor do atributo "l:var" igual ao nome da variável.
@@ -50,13 +102,13 @@ class Leve {
         let filhos = pai.getElementsByTagName("span");
         
         // Percorre cada tag <span> "filha" de um elemento "pai".
-        for (let span of filhos) {
+        for(let span of filhos) {
             // Percorre os nomes das variáveis.
-            for (let nomeVar of Object.keys(this._variaveis)) {
+            for(let nomeVar of Object.keys(this._variaveis)) {
                 // Se o valor do atributo "l:var" de uma tag <span> for igual ao nome de uma variável.
-                if (span.getAttribute("l:var") == nomeVar) {
+                if(span.getAttribute("l:var") == nomeVar) {
                     // O conteúdo daquela tag <span> torna-se o valor daquela variável.
-                    span.innerHTML = this._variaveis[nomeVar];
+                    span.innerHTML = this[nomeVar];
                 }
             }
         }
