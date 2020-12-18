ab28a39962db14c0a3aa4a0fdce4277bb8693aaf
crisstinasantos <69328575+crisstinasantos@users.noreply.github.com>
2020-12-18 17:34:18 -0300

    issue #4

--- testes/leve.js
+++ testes/leve.js
@@ -1,7 +1,35 @@
-setHTML(id,textHTML)
-{
-        let _id = document.getElementById(id);
-        _id.innerHTML = textHTML; 
-         }
+atualizarVariavel(id,variavel)
 
-
+    {  
+		let vet = [];
+            //valor máximo da quantidade de caracteres
+ 		let max = this.obs.length;
+            
+		vet = this.obs;
+     
+		setIntervalo(
+			    //plugin editor typescript
+            function tss()
+                  {
+				for(let x of document.getElementById(id).children)
+                  {
+					if(x.getAttribute('l:bind') != null)
+                        {
+						for(let i=0; i<max ;i++)    //percorrer String
+                            {
+          
+                                  let str =	vet[i].innerHTML;
+                                           if(x.value=="")
+                                                {
+           		                                        break;
+						}
+                             let aj = str.replace(variavel,x.value);       // substituir onde tem variavel por valor de x
+           
+          	                        vet[i].innerHTML = aj;
+                                             variavel = x.value;
+           
+						                    }
+					                    }
+				                     });
+			                    }
+	                    	}
