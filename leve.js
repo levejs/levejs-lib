23fc70af897c8e739faabff807f9d54edc47f933
crisstinasantos <69328575+crisstinasantos@users.noreply.github.com>
2020-12-18 17:22:40 -0300

    issue #5

--- leve.js
+++ leve.js
@@ -1,35 +1,27 @@
-atualizarVariavel(id,variavel)
-
-    {  
+comunicacao(){
 		let vet = [];
-            //valor máximo da quantidade de caracteres
- 		let max = this.obs.length;
-            
-		vet = this.obs;
-     
-		setIntervalo(
-			    //plugin editor typescript
-            function tss()
-                  {
-				for(let x of document.getElementById(id).children)
-                  {
-					if(x.getAttribute('l:bind') != null)
+            //valor maximo quantidade de caracteres
+		let max = this.obs.length;
+		
+        vet = this.obs;
+		
+        setInterval((
+			                
+                    function r()
+                                {
+				        for(let x of document.getElementById(this.noticaId()).children)
+                                {
+					if(x.getAttribute('l:bind') != undefined)
                         {
-						for(let i=0; i<max ;i++)    //percorrer String
-                            {
-          
-                                  let str =	vet[i].innerHTML;
-                                           if(x.value=="")
-                                                {
-           		                                        break;
-						}
-                             let aj = str.replace(variavel,x.value);       // substituir onde tem variavel por valor de x
-           
-          	                        vet[i].innerHTML = aj;
-                                             variavel = x.value;
-           
-						                    }
-					                    }
-				                     });
-			                    }
-	                    	}
+						document.getElementById("let").addEventListener("click", function() 
+                                    {                                   //adiciona um evento ao click do botão 	
+							for(let i=0;i<max;i++)  
+                                    {
+								vet[i].innerHTML = x.value;         //modifica substitui o conteudo html pelo valor do input
+							}
+						}); 
+					}
+				}
+			}
+		),100);    
+	}
