dc52b02eb6409244fbc703f57a29869e058a2916
crisstinasantos <69328575+crisstinasantos@users.noreply.github.com>
2020-12-18 17:20:22 -0300

    issue #4

--- leve.js
+++ leve.js
@@ -1,78 +1,35 @@
+atualizarVariavel(id,variavel)
 
-class Local{
-    lit<Voltar> lista = new ArrayList <Voltar>();
-
-    add(Local estado){
-        lista.add(estado);
-}
-    Voltar get(posicao){
-        lista.get(posicao);
-    }
-    
-    class Voltar{
-         let estado;
-
-    Voltar(estado){
-             this.estado = estado;
-    }
-
-
-    getEstado(){
-        estado;
-    }
-
-
-        class origem{
-            let string estado;
-    
-         setEstado(String salvarEstado){
-             this.estado = salvarEstado;
-        }
-   
-
-         getEstado(){
-            return this.estado;
-    }
-
-
-    salvarEstado(){
-        return new ve(estado);
-    }
-
-
-        estadoSalvo(ve ultimoEstado){                       
-            estado = ultmoEstado.getEstado();
-    }
-
-
-
-class Leve{
-    Origem origem = new Origem();
-    Local local = new Local();
-
-    origem.setEstado("Estado inicial");
-    local.add(origem.salvarEstado());
-
-    origem.setEstado("Proximo estado");
-    local.add(origem.salvarEstado());
-
-    origem.setEstado("Estado atual");
-    window.print("Estado atual");
-    console.log(origem.getEstado());
-
-    origem.getEstadoSalvo(local.get(0));
-    window.print("Estado incial");
-    console.log(origem.getEstado());
-    
-    origem.getEstadoSalvo(local.get(1));
-    window.print("proximo estado");
-    console.log(origem.getEstado());
-
-}
-
-
-
-
-
-}
-    
+    {  
+		let vet = [];
+            //valor máximo da quantidade de caracteres
+ 		let max = this.obs.length;
+            
+		vet = this.obs;
+     
+		setIntervalo(
+			    //plugin editor typescript
+            function tss()
+                  {
+				for(let x of document.getElementById(id).children)
+                  {
+					if(x.getAttribute('l:bind') != null)
+                        {
+						for(let i=0; i<max ;i++)    //percorrer String
+                            {
+          
+                                  let str =	vet[i].innerHTML;
+                                           if(x.value=="")
+                                                {
+           		                                        break;
+						}
+                             let aj = str.replace(variavel,x.value);       // substituir onde tem variavel por valor de x
+           
+          	                        vet[i].innerHTML = aj;
+                                             variavel = x.value;
+           
+						                    }
+					                    }
+				                     });
+			                    }
+	                    	}
